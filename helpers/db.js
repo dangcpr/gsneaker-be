082a00ba6c73d1e2e6ce9afed33a4dcc614f0bba
Nguyễn Hải Đăng <nguyenthiminhkhai63@gmail.com>
2023-07-11 18:27:10 +0700

    Change deployment service

--- helpers/db.js
+++ helpers/db.js
@@ -3,11 +3,11 @@ const initOptions = {};
 const pgp = require("pg-promise")(initOptions);
 
 const db = {
-    host: "ep-polished-cake-891996-pooler.ap-southeast-1.postgres.vercel-storage.com",
+    host: "dpg-cikgvctgkuvinfj2583g-a.singapore-postgres.render.com",
     port: 5432,
-    database: "verceldb",
-    user: "default",
-    password: "IG5SriYU4hsf",
+    database: "gsneaker",
+    user: "admin",
+    password: "aqSntS2TXY245sLXDhAEQVjaJmLEvori",
     ssl: true,
     max: 20,
 }
